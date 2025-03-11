b86b4c29d542fcdfe32202b1afe4d57ae0e17ae0
BSlaven <slaven.bunijevac@gmail.com>
2025-03-11 23:16:23 +0100

    add icons to dice values

--- src/components/Dice/dice.jsx
+++ src/components/Dice/dice.jsx
@@ -3,12 +3,12 @@ import { useEffect, useState } from "react";
 import { CgDice2, CgDice1, CgDice3, CgDice4, CgDice5, CgDice6 } from "react-icons/cg";
 
 const diceIcons = {
-  input_dice_one: <CgDice1 />,
-  input_dice_two: <CgDice2 />,
-  input_dice_three: <CgDice3 />,
-  input_dice_four: <CgDice4 />,
-  input_dice_five: <CgDice5 />,
-  input_dice_six: <CgDice6 />
+  input_dice_1: <CgDice1 />,
+  input_dice_2: <CgDice2 />,
+  input_dice_3: <CgDice3 />,
+  input_dice_4: <CgDice4 />,
+  input_dice_5: <CgDice5 />,
+  input_dice_6: <CgDice6 />
 }
 
 const Dice = () => {
@@ -19,32 +19,38 @@ const Dice = () => {
     input_dice_one: {
       label: 'input_dice_one',
       value: 1,
-      checked: false
+      checked: false,
+      icon: diceIcons.input_dice_1
     },
     input_dice_two: {
       label: 'input_dice_two',
       value: 2,
-      checked: true
+      checked: true,
+      icon: diceIcons.input_dice_2
     },
     input_dice_three: {
       label: 'input_dice_three',
       value: 3,
-      checked: false
+      checked: false,
+      icon: diceIcons.input_dice_3
     },
     input_dice_four: {
       label: 'input_dice_four',
       value: 4,
-      checked: true
+      checked: true,
+      icon: diceIcons.input_dice_4
     },
     input_dice_five: {
       label: 'input_dice_five',
       value: 5,
-      checked: true
+      checked: true,
+      icon: diceIcons.input_dice_5
     },
     input_dice_six: {
       label: 'input_dice_six',
       value: 6,
-      checked: false
+      checked: false,
+      icon: diceIcons.input_dice_6
     },
 });
 
@@ -62,7 +68,8 @@ const Dice = () => {
       newDiceValues[item[0]] = {
         checked: false,
         value: newRandomValue,
-        label: item[0]
+        label: item[0],
+        icon: diceIcons[`input_dice_${newRandomValue}`]
       }
     })
     
@@ -80,7 +87,9 @@ const Dice = () => {
     <section className='dice_cointainer'>
       <div className="dice">
         <div className="input_control">
-          <label htmlFor="input_dice_one"></label>
+          <label htmlFor="input_dice_one">
+            {currentDice.input_dice_one.icon}
+          </label>
           <input
             type="checkbox"
             name="input_dice_one"
@@ -88,35 +97,45 @@ const Dice = () => {
           />
         </div>
         <div className="input_control">
-          <label htmlFor="input_dice_two"></label>
+          <label htmlFor="input_dice_two">
+          {currentDice.input_dice_two.icon}
+          </label>
           <input 
             type="checkbox"
             name="input_dice_two"
             id="input_dice_two" />
         </div>
         <div className="input_control">
-          <label htmlFor="input_dice_three"></label>
+          <label htmlFor="input_dice_three">
+            {currentDice.input_dice_three.icon}
+          </label>
           <input 
             type="checkbox"
             name="input_dice_three"
             id="input_dice_three" />
         </div>
         <div className="input_control">
-          <label htmlFor="input_dice_four"></label>
+          <label htmlFor="input_dice_four">
+            {currentDice.input_dice_four.icon}
+          </label>
           <input 
             type="checkbox"
             name="input_dice_four"
             id="input_dice_four" />
         </div>
         <div className="input_control">
-          <label htmlFor="input_dice_five"></label>
+          <label htmlFor="input_dice_five">
+            {currentDice.input_dice_five.icon}
+          </label>
           <input 
             type="checkbox"
             name="input_dice_five"
             id="input_dice_five" />
         </div>
         <div className="input_control">
-          <label htmlFor="input_dice_six"></label>
+          <label htmlFor="input_dice_six">
+            {currentDice.input_dice_six.icon}
+          </label>
           <input 
             type="checkbox"
             name="input_dice_six"
