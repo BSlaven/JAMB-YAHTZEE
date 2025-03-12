39b9e990e4f536c6d8b8b01b103b30aa7f491f1b
BSlaven <slaven.bunijevac@gmail.com>
2025-03-12 20:35:04 +0100

    add styling for checked and unchecked dice

--- src/components/Dice/dice.jsx
+++ src/components/Dice/dice.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import classes from './Dice.module.css';
+
 import { CgDice2, CgDice1, CgDice3, CgDice4, CgDice5, CgDice6 } from "react-icons/cg";
 
 const diceIcons = {
@@ -99,40 +101,40 @@ const Dice = () => {
   
   return (
     <section className='dice_section'>
-      <div className="dice_container">
+      <div className={classes.diceContainer}>
         <div
-          onClick={() => checkDice('input_dice_one')} 
-          className={`single_dice_container ${currentDice.input_dice_one.checked ? 'checked' : null}`}
+          onClick={() => checkDice('input_dice_one')}
+          className={`${classes.singleDiceContainer} ${currentDice.input_dice_one.checked ? classes.checked : null}`}
         >
           {currentDice.input_dice_one.icon}
         </div>
         <div 
-          onClick={() => checkDice('input_dice_two')} 
-          className={`single_dice_container ${currentDice.input_dice_two.checked ? 'checked' : null}`}
+          onClick={() => checkDice('input_dice_two')}
+          className={`${classes.singleDiceContainer} ${currentDice.input_dice_two.checked ? classes.checked : null}`}
         >
           {currentDice.input_dice_two.icon}
         </div>
         <div 
-          onClick={() => checkDice('input_dice_three')} 
-          className={`single_dice_container ${currentDice.input_dice_three.checked ? 'checked' : null}`}
+          onClick={() => checkDice('input_dice_three')}
+          className={`${classes.singleDiceContainer} ${currentDice.input_dice_three.checked ? classes.checked : null}`}
         >
           {currentDice.input_dice_three.icon}
         </div>
         <div 
-          onClick={() => checkDice('input_dice_four')} 
-          className={`single_dice_container ${currentDice.input_dice_four.checked ? 'checked' : null}`}
+          onClick={() => checkDice('input_dice_four')}
+          className={`${classes.singleDiceContainer} ${currentDice.input_dice_four.checked ? classes.checked : null}`}
         >
           {currentDice.input_dice_four.icon}
         </div>
         <div
-          onClick={() => checkDice('input_dice_five')} 
-          className={`single_dice_container ${currentDice.input_dice_five.checked ? 'checked' : null}`}
+          onClick={() => checkDice('input_dice_five')}
+          className={`${classes.singleDiceContainer} ${currentDice.input_dice_five.checked ? classes.checked : null}`}
         >
           {currentDice.input_dice_five.icon}
         </div>
         <div
-          onClick={() => checkDice('input_dice_six')} 
-          className={`single_dice_container ${currentDice.input_dice_six.checked ? 'checked' : null}`}
+          onClick={() => checkDice('input_dice_six')}
+          className={`${classes.singleDiceContainer} ${currentDice.input_dice_six.checked ? classes.checked : null}`}
         >
           {currentDice.input_dice_six.icon}
         </div>
