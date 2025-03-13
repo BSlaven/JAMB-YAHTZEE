e344964617fdda0a298d004e32217aa0e2ad54e0
BSlaven <slaven.bunijevac@gmail.com>
2025-03-13 19:44:14 +0100

    add roll button styling<

--- src/components/Dice/dice.jsx
+++ src/components/Dice/dice.jsx
@@ -100,53 +100,51 @@ const Dice = () => {
   }
   
   return (
-    <section className='dice_section'>
-      <div className={classes.diceContainer}>
-        <div
-          onClick={() => checkDice('input_dice_one')}
-          className={`${classes.singleDiceContainer} ${currentDice.input_dice_one.checked ? classes.checked : null}`}
-        >
-          {currentDice.input_dice_one.icon}
-        </div>
-        <div 
-          onClick={() => checkDice('input_dice_two')}
-          className={`${classes.singleDiceContainer} ${currentDice.input_dice_two.checked ? classes.checked : null}`}
-        >
-          {currentDice.input_dice_two.icon}
-        </div>
-        <div 
-          onClick={() => checkDice('input_dice_three')}
-          className={`${classes.singleDiceContainer} ${currentDice.input_dice_three.checked ? classes.checked : null}`}
-        >
-          {currentDice.input_dice_three.icon}
-        </div>
-        <div 
-          onClick={() => checkDice('input_dice_four')}
-          className={`${classes.singleDiceContainer} ${currentDice.input_dice_four.checked ? classes.checked : null}`}
-        >
-          {currentDice.input_dice_four.icon}
-        </div>
-        <div
-          onClick={() => checkDice('input_dice_five')}
-          className={`${classes.singleDiceContainer} ${currentDice.input_dice_five.checked ? classes.checked : null}`}
-        >
-          {currentDice.input_dice_five.icon}
-        </div>
-        <div
-          onClick={() => checkDice('input_dice_six')}
-          className={`${classes.singleDiceContainer} ${currentDice.input_dice_six.checked ? classes.checked : null}`}
-        >
-          {currentDice.input_dice_six.icon}
-        </div>
+    <div className={classes.diceContainer}>
+      <div
+        onClick={() => checkDice('input_dice_one')}
+        className={`${classes.singleDiceContainer} ${currentDice.input_dice_one.checked ? classes.checked : null}`}
+      >
+        {currentDice.input_dice_one.icon}
+      </div>
+      <div 
+        onClick={() => checkDice('input_dice_two')}
+        className={`${classes.singleDiceContainer} ${currentDice.input_dice_two.checked ? classes.checked : null}`}
+      >
+        {currentDice.input_dice_two.icon}
+      </div>
+      <div 
+        onClick={() => checkDice('input_dice_three')}
+        className={`${classes.singleDiceContainer} ${currentDice.input_dice_three.checked ? classes.checked : null}`}
+      >
+        {currentDice.input_dice_three.icon}
+      </div>
+      <div 
+        onClick={() => checkDice('input_dice_four')}
+        className={`${classes.singleDiceContainer} ${currentDice.input_dice_four.checked ? classes.checked : null}`}
+      >
+        {currentDice.input_dice_four.icon}
+      </div>
+      <div
+        onClick={() => checkDice('input_dice_five')}
+        className={`${classes.singleDiceContainer} ${currentDice.input_dice_five.checked ? classes.checked : null}`}
+      >
+        {currentDice.input_dice_five.icon}
+      </div>
+      <div
+        onClick={() => checkDice('input_dice_six')}
+        className={`${classes.singleDiceContainer} ${currentDice.input_dice_six.checked ? classes.checked : null}`}
+      >
+        {currentDice.input_dice_six.icon}
       </div>
       <button 
-        className="roll_button"
+        className={classes.rollButton}
         onClick={() => rollDice()}
         disabled={rollNumber > 3}
       >
         ROLL {rollNumber <= 3 ? rollNumber : 3}
       </button>
-    </section>
+    </div>
   )
 }
 
