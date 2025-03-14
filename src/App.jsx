532981214ed1557002fb252ed651dccd7c994016
BSlaven <slaven.bunijevac@gmail.com>
2025-03-14 20:21:42 +0100

    add home page link to game title

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router";
 
 import './App.css';
 
@@ -8,7 +8,11 @@ import Game from "./components/Game/Game";
 function App() {
   return (
     <Router>
-      <h1 className='game_title'>IGRAJTE JAMB</h1>
+      <h1 className='game_title'>
+        <Link to={'/'}>
+          IGRAJTE JAMB
+        </Link>
+      </h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game/:id" element={<Game />} />
