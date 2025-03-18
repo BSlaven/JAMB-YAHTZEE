25e195abc4f35afdecdd9e8a2b0c8c52c19f58c2
BSlaven <slaven.bunijevac@gmail.com>
2025-03-18 23:31:31 +0100

    display number fields

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,6 +1,14 @@
 const Column = () => {
   return (
-    <div>Column</div>
+    <section>
+      <h4>d</h4>
+      <div className="field">1</div>
+      <div className="field">2</div>
+      <div className="field">3</div>
+      <div className="field">4</div>
+      <div className="field">5</div>
+      <div className="field">6</div>
+    </section>
   )
 }
 
