dd4ae4f6e1e662370e9a6fea626eb812fd9208c1
BSlaven <slaven.bunijevac@gmail.com>
2025-03-25 23:00:09 +0100

    wrap context around the app and import columns from the context

--- src/components/Fields/Fields.jsx
+++ src/components/Fields/Fields.jsx
@@ -1,15 +1,26 @@
+import { useContext } from 'react';
+
+import DiceContext from '../../context/DiceContext';
+
 import classes from './Fields.module.css';
 
 import Column from '../Column/Column';
 
-const columns = [ 'downColumn', 'freeColumn', 'upColumn', 'announcementColumn']
+// const columns = [ 'downColumn', 'freeColumn', 'upColumn', 'announcementColumn']
 
 const Fields = () => {
+
+  const { gameColumns } = useContext(DiceContext);
+
+  console.log(gameColumns);  
   
   return (
     <div className={classes.fieldsContainer}>
-      {columns.map(column => {
-        return <Column key={column} column={column} />
+      {gameColumns.map(column => {
+        return <Column 
+          key={column.columnName}
+          column={column.columnName}
+        />
       })}
     </div>
   )
