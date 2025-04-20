99c4f33c185d29a77871af9c20a60fccaea5a43c
BSlaven <slaven.bunijevac@gmail.com>
2025-04-20 14:18:27 +0200

    introduce toaster component

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,11 +2,20 @@ import classes from './Game.module.css';
 
 import Dice from '../Dice/Dice';
 import Fields from '../Fields/Fields';
+import { toast, Toaster } from 'sonner';
 
 const Game = () => {
 
   return (
     <div className={classes.gameContainer}>
+
+      <Toaster visibleToasts={5} richColors position='top-left' />
+      <button
+        onClick={() => {
+          return toast.success('dobro je majstore!')
+        }}
+      >toast</button>
+      
       <Dice />
       <Fields />
     </div>
