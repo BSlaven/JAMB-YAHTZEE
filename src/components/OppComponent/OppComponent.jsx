2c50714f323e884044242eb79b15daf094caed02
BSlaven <slaven.bunijevac@gmail.com>
2025-12-11 23:06:31 +0100

    move opponent component into separate section

--- src/components/OppComponent/OppComponent.jsx
+++ src/components/OppComponent/OppComponent.jsx
@@ -1,40 +1,49 @@
 import { useState, useEffect } from 'react';
 
-import { io } from 'socket.io-client';
+import Dice from '../Dice/Dice';
+import Fields from '../Fields/Fields';
 
-const socket = io("http://localhost:3000", {
-  withCredentials: true
-});
+import classes from './OppComponent.module.css'
+
+// import { io } from 'socket.io-client';
+
+// const socket = io("http://localhost:3000", {
+//   withCredentials: true
+// });
 
 const OppComponent = () => {
 
   const [ opponentData, setOpponentData ] = useState(null);
   const [ opponentDice, setOpponentDice ] = useState(null);
 
-  useEffect(() => {
-    socket.on('opponentData', (data) => {
-      setOpponentData(data);
-    });
+  // useEffect(() => {
+  //   socket.on('opponentData', (data) => {
+  //     setOpponentData(data);
+  //   });
 
-    socket.on('newDiceValues', (data) => {
-      console.log('New dice values received:', data);
-      setOpponentDice(data);
-    });
+  //   socket.on('newDiceValues', (data) => {
+  //     console.log('New dice values received:', data);
+  //     setOpponentDice(data);
+  //   });
 
-    return () => {
-      socket.off('opponentData');
-    }
-  }, [])
+  //   return () => {
+  //     socket.off('opponentData');
+  //   }
+  // }, [])
 
   return (
     <div>
-      {opponentData && <div>
+      {/* {opponentData && <div>
         <h5>Opponent Data</h5>
         <p>Opponent numbers: {opponentData.columnNumbersTotal}</p>
         <p>Opponent diff: {opponentData.columnDifference}</p>
         <p>Opponent sets: {opponentData.columnSetsTotal}</p>
         <p>Opponent totals: {opponentData.columnNumbersTotal + opponentData.columnDifference + opponentData.columnSetsTotal}</p>
-        </div>}
+        </div>} */}
+        <section className={classes.opponentContainer}>
+          <Dice />
+          <Fields isOpponent={true} />
+        </section>
     </div>
   )
 }
