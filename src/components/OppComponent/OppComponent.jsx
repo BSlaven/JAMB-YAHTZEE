1beccd950a99899dd36b6adf0d06350dda933c27
BSlaven <slaven.bunijevac@gmail.com>
2025-12-18 16:34:57 +0100

    display opponent data

--- src/components/OppComponent/OppComponent.jsx
+++ src/components/OppComponent/OppComponent.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect,useContext } from 'react';
 
 import Dice from '../Dice/Dice';
 import Fields from '../Fields/Fields';
 
-import classes from './OppComponent.module.css'
+import classes from './OppComponent.module.css';
+
+import DiceContext from "../../context/DiceContext";
 
 // import { io } from 'socket.io-client';
 
@@ -11,7 +13,20 @@ import classes from './OppComponent.module.css'
 //   withCredentials: true
 // });
 
+// const diceIcons = {
+//   input_dice_1: <CgDice1 />,
+//   input_dice_2: <CgDice2 />,
+//   input_dice_3: <CgDice3 />,
+//   input_dice_4: <CgDice4 />,
+//   input_dice_5: <CgDice5 />,
+//   input_dice_6: <CgDice6 />
+// }
+
 const OppComponent = () => {
+
+  const { dice, diceIcons } = useContext(DiceContext);
+
+  const [ rollNumber, setRollNumber ] = useState(3)
 
   const [ opponentData, setOpponentData ] = useState(null);
   const [ opponentDice, setOpponentDice ] = useState(null);
@@ -41,11 +56,57 @@ const OppComponent = () => {
         <p>Opponent totals: {opponentData.columnNumbersTotal + opponentData.columnDifference + opponentData.columnSetsTotal}</p>
         </div>} */}
         <section className={classes.opponentContainer}>
-          <Dice />
+          <div className={classes.diceContainer}>
+            {
+              Object.entries(dice).map(([name, oneDie]) => (
+                <div
+              className={`${classes.singleDiceContainer} ${dice.input_dice_two.checked ? classes.checked : null}`}
+            >
+              {diceIcons[`input_dice_${oneDie.value}`]}
+            </div>
+              ))
+            }
+            {/* <div
+              className={`${classes.singleDiceContainer} ${dice.input_dice_two.checked ? classes.checked : null}`}
+            >
+              {diceIcons.input_dice_1}
+            </div>
+            <div 
+              className={`${classes.singleDiceContainer} ${dice.input_dice_two.checked ? classes.checked : null}`}
+            >
+              {diceIcons.input_dice_2}
+            </div>
+            <div 
+              className={`${classes.singleDiceContainer} ${dice.input_dice_three.checked ? classes.checked : null}`}
+            >
+              {diceIcons.input_dice_3}
+            </div>
+            <div 
+              className={`${classes.singleDiceContainer} ${dice.input_dice_four.checked ? classes.checked : null}`}
+            >
+              {diceIcons.input_dice_4}
+            </div>
+            <div
+              className={`${classes.singleDiceContainer} ${dice.input_dice_five.checked ? classes.checked : null}`}
+            >
+              {diceIcons.input_dice_5}
+            </div>
+            <div
+              className={`${classes.singleDiceContainer} ${dice.input_dice_six.checked ? classes.checked : null}`}
+            >
+              {diceIcons.input_dice_6}
+            </div> */}
+            <button 
+              className={classes.rollButton}
+            >
+              ROLL {rollNumber <= 3 ? rollNumber : 3}
+            </button>
+          </div>
+          {/* <Dice /> */}
           <Fields isOpponent={true} />
         </section>
     </div>
   )
 }
 
-export default OppComponent
+export default OppComponent;
