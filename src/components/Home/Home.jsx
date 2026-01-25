6938a42829ed13e33f2abfeab51872f19c2d4f42
BSlaven <slaven.bunijevac@gmail.com>
2026-01-25 20:48:57 +0100

    add toaster to Home component structure

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router";
 
-import { Toaster, toast } from 'sonner';
+import { Toaster } from 'sonner';
 
 import classes from './Home.module.css';
 
@@ -9,7 +9,6 @@ import DiceContext from '../../context/DiceContext';
 
 const Home = () => {
 
-  // <Toaster visibleToasts={5} richColors position='top-left' />
 
   const navigate = useNavigate();
 
@@ -31,51 +30,55 @@ const Home = () => {
 
   
   return (
-    <div className={classes.inputControl}>
-      <label htmlFor="game_id">Unesite ID igre</label>
-      <input
-        onChange={(e) => setGameId(e.target.value)}
-        value={gameId}
-        type="text"
-        id="game_id"
-        name="game_id"
-        placeholder="id igre..."
-      />
+    <>
+      <Toaster visibleToasts={5} richColors position='top-left' />
 
-      <input type="checkbox" id="downColumn" name="downColumn" />
-      <label htmlFor="downColumn">Dole</label>
+      <div className={classes.inputControl}>
+        <label htmlFor="game_id">Unesite ID igre</label>
+        <input
+          onChange={(e) => setGameId(e.target.value)}
+          value={gameId}
+          type="text"
+          id="game_id"
+          name="game_id"
+          placeholder="id igre..."
+        />
 
-      <input type="checkbox" id="freeColumn" name="freeColumn" />
-      <label htmlFor="freeColumn">Sloboda</label>
-      
-      <input type="checkbox" id="upColumn" name="upColumn" />
-      <label htmlFor="upColumn">Gore</label>
+        <input type="checkbox" id="downColumn" name="downColumn" />
+        <label htmlFor="downColumn">Dole</label>
 
-      <input type="checkbox" id="announcementColumn" name="announcementColumn" />
-      <label htmlFor="announcementColumn">Najava</label>
+        <input type="checkbox" id="freeColumn" name="freeColumn" />
+        <label htmlFor="freeColumn">Sloboda</label>
+        
+        <input type="checkbox" id="upColumn" name="upColumn" />
+        <label htmlFor="upColumn">Gore</label>
 
-      <input type="checkbox" id="fromMiddleColumn" name="fromMiddleColumn" />
-      <label htmlFor="fromMiddleColumn">Od sredine</label>
-      
-      <input type="checkbox" id="fromMiddleColumn" name="fromMiddleColumn" />
-      <label htmlFor="fromMiddleColumn">Do sredine</label>
+        <input type="checkbox" id="announcementColumn" name="announcementColumn" />
+        <label htmlFor="announcementColumn">Najava</label>
 
-      <input type="checkbox" id="handColumn" name="handColumn" />
-      <label htmlFor="handColumn">Ručna</label>
+        <input type="checkbox" id="fromMiddleColumn" name="fromMiddleColumn" />
+        <label htmlFor="fromMiddleColumn">Od sredine</label>
+        
+        <input type="checkbox" id="fromMiddleColumn" name="fromMiddleColumn" />
+        <label htmlFor="fromMiddleColumn">Do sredine</label>
 
-      <input type="checkbox" id="answerColumn" name="answerColumn" />
-      <label htmlFor="answerColumn">Odjava</label>
+        <input type="checkbox" id="handColumn" name="handColumn" />
+        <label htmlFor="handColumn">Ručna</label>
 
-      <input type="checkbox" id="maximumColumn" name="maximumColumn" />
-      <label htmlFor="maximumColumn">Maksimum</label>
+        <input type="checkbox" id="answerColumn" name="answerColumn" />
+        <label htmlFor="answerColumn">Odjava</label>
 
-      <button 
-        className={classes.enterGameBtn}
-        onClick={enterGameHandler}
-      >
-        Uđi u igru
-      </button>
-    </div>
+        <input type="checkbox" id="maximumColumn" name="maximumColumn" />
+        <label htmlFor="maximumColumn">Maksimum</label>
+
+        <button 
+          className={classes.enterGameBtn}
+          onClick={enterGameHandler}
+        >
+          Uđi u igru
+        </button>
+      </div>
+    </>
   )
 }
 
