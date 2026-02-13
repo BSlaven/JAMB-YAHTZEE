5c76ad5c03df7b8c62e43d80465475bc3b7926da
BSlaven <slaven.bunijevac@gmail.com>
2026-02-13 21:29:36 +0100

    add links to navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router";
+
+
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
   return (
     <nav className={classes.navbar}>
-      
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/game">Game</Link></li>
+        <li><Link to="/rules">Rules</Link></li>
+      </ul>
     </nav>
   )
 }
